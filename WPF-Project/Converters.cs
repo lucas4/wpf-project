a119c6cbc3d07d9724298b37756c0c39c7384b3d
lucas4 <troskolukasz@gmail.com>
2016-05-30 10:11:18 +0200

    converters

---
+++ WPF-Project/Converters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Project
+{
+    public class todayConverter : IValueConverter
+    {
+        enum Days { Normal, Today, Other };
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+                return Days.Normal;
+
+            try
+            {
+                int dValue = Int32.Parse(value.ToString());
+                if (dValue < 5)
+                    return Days.Other;
+                else if (dValue == 15) // if its today date 
+                    return Days.Today;
+                else
+                    return Days.Normal;
+            }
+            catch
+            {
+                return Days.Normal;
+            }
+
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+
+    }
+}
