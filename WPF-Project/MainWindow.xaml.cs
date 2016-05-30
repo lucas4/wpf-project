6590b0df68fcc302abb330c8f395a98aceb73e15
lucas4 <troskolukasz@gmail.com>
2016-05-30 09:58:26 +0200

    first changes

--- WPF-Project/MainWindow.xaml.cs
+++ WPF-Project/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace WPF_Project
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            for (int j = 0; j < 4; j++)
+            {
+                Week w = new Week();
+                for (int i = 0; i < 7; i++)
+                {
+                    w.day[i] = (i+1) + (j*7) + "";
+                }
+                dat.Items.Add(w);
+            }
+            
+            
+
+
+        }
+
+        private void dat_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
         }
     }
+
+    public class Week
+    {
+        public string[] day { get; set; }
+        public Week()
+        {
+            day = new string[7];
+        }
+
+    }
+
+
+    
 }
